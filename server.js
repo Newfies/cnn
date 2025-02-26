2e7123dfcea24c9d15c7c38be918caaedb532492
PrivacySecured <LoveNewfies@proton.me>
2025-02-25 19:38:29 -0600

    d

--- server.js
+++ server.js
@@ -11,48 +11,41 @@ const moment = require('moment-timezone');
 const randomstring = require("randomstring");
 const fetch = require('node-fetch');
 const { createCanvas } = require('canvas');
-const njs = require("newfiesjs");
-
-njs.config('reminderConfig', false);
-
-
-dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config()
 
 /* Other Variables */
 let timezone = process.env.TIMEZONE || "America/Chicago";
 let rawCurrentDateTime = new Date();
 let currentDateTime = moment(rawCurrentDateTime).tz(timezone).format('MMMM Do YYYY, h:mm:ss a');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 377;
 
-/* Paths */
-app.use(express.static(path.join(__dirname, 'public')));
-
-/* Set */
-// Set the view engine to ejs
 app.set('view engine', 'ejs');
-// Define the folder where the ejs files will be stored
 app.set('views', path.join(__dirname, '/views'));
-
-/* Use */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({
-  secret: process.env.SECRET_SESSION,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(session({ secret: process.env.SECRET_SESSION, resave: false, saveUninitialized: true }));
+
+/* Post */
+app.post('/home', function(req, res) {
+    const passcode = req.body.lis;
+    if (passcode == process.env.PASSCODE){
+        req.session.access = "verified"
+        res.redirect("/home")
+    } else {
+        res.redirect(301, "https://www.cnn.com/error");
+    }
+});
 
 /* Get */
-// Redirects Users From "/" to "/home"
 app.get('/', function(req, res) {
     res.redirect("/home");
 });
 
 app.get('/home', function(req, res){
-    if (req.session.a){
-        if (req.session.a == "verified"){
-            res.render('x')
+    if (req.session.access){
+        if (req.session.access == "verified"){
+            res.render('website')
         } else {
             res.render('home');
             return
@@ -63,14 +56,17 @@ app.get('/home', function(req, res){
 });
 
 app.get('*', function(req, res) {
-    res.redirect("/home");
-});
-
-app.post('/home', function(req, res) {
-    const x = req.body.lis;
-    if (x == "1204"){
-        req.session.a = "verified"
-        res.redirect("/home")
+    if (req.session.access){
+        if (req.session.access = "verified"){
+            res.send(`Invalid Location, Click Button To Go Back, <button onclick="window.history.back()">Go Back</button>`)
+            return
+        } else {
+            res.redirect(301, "https://www.cnn.com/error");
+            return;
+        }
+    } else {
+        res.redirect(301, "https://www.cnn.com/error");
+        return;
     }
 });
 
